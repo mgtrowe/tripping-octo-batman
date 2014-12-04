51a39114e4991184494e8b958bea9c78cf3ea6cf
Martin Rowe <MRowe@Martins-MacBook-Pro.local>
2014-12-04 07:25:04 +0000

    Circular buffer, variable channel count, making an output file - but at the moment it's just noise...

--- cw2_MartinRowe.c
+++ cw2_MartinRowe.c
@@ -15,39 +15,46 @@ description of program
 #include "portsf.h"
 #include "CW2_Biquads.h"
 
-#define NUM_SAMPLES_IN_FRAME 1024
+#define NUM_SAMPLES_IN_BLOCK 1024
 #define USAGE "cw2_MartinRowe <input wav> <output wav> <cutoff frequency in Hz>"
 
-// To ensure that we do not use portsf to close a file that was never opened.
+// To ensure that portsf does not close a file that was never opened.
 #define INVALID_PORTSF_FID -1
 
+//circulr buffer
+#define CIRCULAR_BUFFER_LENGTH 1024
+
+// To make calls to portsf more readable
 enum float_clipping { DO_NOT_CLIP_FLOATS, CLIP_FLOATS };
 enum minheader { DO_NOT_MINIMISE_HDR, MINIMISE_HDR };
 enum auto_rescale { DO_NOT_AUTO_RESCALE, AUTO_RESCALE };  
 
-int isWav(char s[]){
-	if (strstr(s, ".wav") != NULL){
-		return 1;
-	} else {
-		return 0;
-	}
-}
 
+//declare functions
+void biquad(
+    float *buffer,
+    float *circBuffer,
+    int *circBufferIndex,
+    long num_frames
+);
+void zero_io_buffer(float *buffer);
+
+
+//---------------------------------------------------------------------
 int main( int argc, char *argv[] ){
-	/* <programme name> Old.wav New.wav 1000 -filtertype highpass */
 
-	/*  PRINT:
-		1. The name of the input file (including the .wav suffix),
-		2. The name of the output file (including the .wav suffix), and 
-		3. The cut-off frequency of the filter (in Hz). */
+    //fIDs are initialised to help with clean up code (if implimented)
 	int in_fID = INVALID_PORTSF_FID;
 	int out_fID = INVALID_PORTSF_FID;
-	PSF_PROPS audio_properties;
-	int return_value = EXIT_SUCCESS;
+
+
+	PSF_PROPS audio_properties; //PSF_props is a type defined in portsf
+    float buffer[NUM_SAMPLES_IN_BLOCK];
+    float circBuffer[CIRCULAR_BUFFER_LENGTH] = {0.0}; //init circular Buffer and set all to zero values
+    int circBufferIndex;
+
+	int return_value = EXIT_SUCCESS; //from stdlib
  	
- 	DWORD nFrames = NUM_SAMPLES_IN_FRAME; 
-	float *frame = NULL;
-    long num_frames_read;
 
 	// Initialise portsf library
     if(psf_init()) {
@@ -63,6 +70,17 @@ int main( int argc, char *argv[] ){
         return_value = EXIT_FAILURE;
     }
 
+    //frame counters 
+    printf("Number of channels in input file: %i\n",audio_properties.chans);
+    DWORD nFrames = NUM_SAMPLES_IN_BLOCK / audio_properties.chans; 
+    float *block = NULL;
+
+    long num_frames_written;
+    long num_frames_read;
+    long num_frames_to_write;
+
+
+
  	// Create the output file
     if ((out_fID = psf_sndCreate(argv[2], &audio_properties,
         CLIP_FLOATS, DO_NOT_MINIMISE_HDR, PSF_CREATE_RDWR))<0) {
@@ -70,22 +88,47 @@ int main( int argc, char *argv[] ){
         printf("Unable to create file %s\n",argv[2]);
         return_value = EXIT_FAILURE;
     }
-    //allocate memory for frame
-   if ((frame = (float*)malloc(nFrames
-        *audio_properties.chans*sizeof(float)))==NULL) {
+    //allocate memory for block
+   if ((block = (float*) malloc(nFrames * audio_properties.chans * sizeof(float)))==NULL) {
 
-        printf("Unable to allocate memory for frame.\n");
+        printf("Unable to allocate memory for block.\n");
         return_value = EXIT_FAILURE;
     }
 
     // Read frames from input file
-    while ((num_frames_read=psf_sndReadFloatFrames(in_fID, frame, nFrames)) > 0) { 
+    while ((num_frames_read=psf_sndReadFloatFrames(in_fID, block, nFrames)) > 0) { 
         
+        //filter signal
+        biquad(buffer,circBuffer,&circBufferIndex,num_frames_read);
+
         // Write the frame to the output file
-        if (psf_sndWriteFloatFrames(out_fID,frame,num_frames_read)!=num_frames_read) {
+        num_frames_written = psf_sndWriteFloatFrames(out_fID,buffer,num_frames_read);
+
+        if (num_frames_written!=num_frames_read) {
             printf("Unable to write to %s\n",argv[2]);
             return_value = EXIT_FAILURE;
-            break;
+        }
+    }
+
+
+    // Deal with leftover frames
+    for (int buffer_num=0; buffer_num<(CIRCULAR_BUFFER_LENGTH-1); buffer_num += nFrames) 
+    {    
+        // Determine how many frames to write from the buffer
+        if ((buffer_num+nFrames)>(CIRCULAR_BUFFER_LENGTH-1)) {
+            num_frames_to_write = (CIRCULAR_BUFFER_LENGTH-1)-buffer_num;
+        } else {
+            num_frames_to_write = nFrames;
+        }
+
+        zero_io_buffer(buffer);
+        biquad(buffer,circBuffer,&circBufferIndex,num_frames_to_write);
+
+        // Write the buffer to the output file
+        num_frames_written = psf_sndWriteFloatFrames(out_fID,buffer,num_frames_to_write);
+        if (num_frames_written!=num_frames_to_write) {
+            printf("Unable to write to %s\n",argv[2]);
+            return_value = EXIT_FAILURE;
         }
     }
 
@@ -93,8 +136,8 @@ int main( int argc, char *argv[] ){
     //cleanup
     
 	   // Free the memory for the frame
-    if (frame)
-        free(frame);
+    if (block)
+        free(block);
 
     // Close the output file
     if (out_fID>=0)
@@ -110,3 +153,25 @@ int main( int argc, char *argv[] ){
 	return 0;
 }
 
+void biquad( float *buffer, float *circBuffer, int *circBufferIndex, long num_frames ) {
+    int next_circBufferIndex;
+    float current_sample;
+    float delayed_sample;
+
+
+    for (int frame_idx=0; frame_idx<num_frames; frame_idx++) {
+        next_circBufferIndex = (*circBufferIndex+1)%CIRCULAR_BUFFER_LENGTH;
+        buffer[frame_idx] = 0.0;
+        // current_sample = buffer[frame_idx];
+        // circBuffer[*circBufferIndex] = current_sample;
+        // delayed_sample = circBuffer[next_circBufferIndex];
+        // // We are using the same buffer for input and output
+        // buffer[frame_idx] = current_sample + (0.5*delayed_sample);
+        *circBufferIndex = next_circBufferIndex;
+    }
+
+}
+
+void zero_io_buffer(float *buffer) {
+    memset(buffer,0,NUM_SAMPLES_IN_BLOCK*sizeof(float));
+}
